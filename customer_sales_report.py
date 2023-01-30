aed8d2bfe6d7eff049c811edbf1395c772a99e73
pwitschonke <preston_witschonke1@baylor.edu>
2023-01-30 16:01:32 -0600

    homework submitted

--- customer_sales_report.py
+++ customer_sales_report.py
@@ -1,14 +1,32 @@
 import csv
 
 with open('sales.csv', 'r') as inputFile:
-    inputFile = csv.reader(inputFile)
+    inputFile = list(csv.reader(inputFile))
+
+    ID = inputFile[1][0]
+    total = 0
+
+    print(ID)
 
     with open("salesreport.csv", "w", newline='') as outputFile:
         outputFile = csv.writer(outputFile)
 
         outputFile.writerow(["CustomerID", "Total"])
-        next(inputFile)
 
-        for line in inputFile:
-            outputFile.writerow(
-                [line[0], round((float(line[3]) + float(line[4]) + float(line[5])), 2)])
+        for i in range(1, len(inputFile)):
+            if ID == inputFile[i][0]:
+                newTotal = float(
+                    inputFile[i][3]) + float(inputFile[i][4]) + float(inputFile[i][5])
+                total += newTotal
+
+            else:
+                outputFile.writerow([inputFile[i-1][0]] +
+                                    [str(round(total, 2))])
+                ID = inputFile[i][0]
+                total = 0
+                newTotal = float(
+                    inputFile[i][3]) + float(inputFile[i][4]) + float(inputFile[i][5])
+                total += newTotal
+
+            if i == len(inputFile) - 1:
+                outputFile.writerow([inputFile[i][0]] + [str(round(total, 2))])
