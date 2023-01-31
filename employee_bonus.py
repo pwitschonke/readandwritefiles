24b5900124c9b7ac43c6c0d71ee8425c17d7436d
pwitschonke <preston_witschonke1@baylor.edu>
2023-01-30 23:14:46 -0600

    updated employee bonus file

--- employee_bonus.py
+++ employee_bonus.py
@@ -5,5 +5,17 @@ with open('EmployeePay.csv', 'r') as inputFile:
 
     next(inputFile)
 
+    print("ID    First Name      Last Name       Salary           Bonus           Total Pay")
+
     for i in inputFile:
-        print(f"{i[0]:5s} {i[1]:15s} {i[2]:15s} {i[3]:15s} {i[4]:15s}")
+        iden = i[0]
+        fName = i[1]
+        lName = i[2]
+        salary = i[3]
+        bonus = i[4]
+        totalPay = float(i[3]) * (1 + float(i[4]))
+
+        print(
+            f"{i[0]:5s} {i[1]:15s} {i[2]:15s} ${i[3]:15s} {i[4]:15s} ${round(totalPay,2)}", end='')
+
+        input()
